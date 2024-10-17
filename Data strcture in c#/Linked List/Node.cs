95c1620b8abd789ea05930cf4ce0db5acf8bdeb6
Michael Maurice <M.Maurice@klickit.co>
2024-10-18 00:09:54 +0300

    sync

---
+++ Data strcture in c#/Linked List/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_strcture_in_c_.Linked_List
+{
+    public class Node<T>
+    {
+        public T data { get; set; }
+        public Node<T>Next { get; set; }
+
+        public Node(T data)
+        {
+            data = data;
+            Next = null;
+        }
+    }
+}
